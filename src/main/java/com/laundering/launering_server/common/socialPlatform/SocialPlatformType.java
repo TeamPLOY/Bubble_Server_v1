3bf3221df661a02de3a12556fc11673490abafa0
noahmik <s46422574@gmail.com>
2024-07-27 13:33:36 +0900

    FEAT :: socialPlatform 디렉토리 구현

---
+++ src/main/java/com/laundering/launering_server/common/socialPlatform/SocialPlatformType.java
@@ -0,0 +1,5 @@
+package com.laundering.launering_server.common.socialPlatform;
+
+public enum SocialPlatformType {
+    BSM
+}
---
+++ src/main/java/com/laundering/launering_server/common/socialPlatform/SocialPlatformUserInfo.java
@@ -0,0 +1,4 @@
+package com.laundering.launering_server.common.socialPlatform;
+
+public record SocialPlatformUserInfo(String userId, String email) {
+}
