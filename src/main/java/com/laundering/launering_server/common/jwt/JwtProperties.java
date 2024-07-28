fda53900478623526a3848f4767a0a316828d794
noahmik <s46422574@gmail.com>
2024-07-28 13:16:25 +0900

    FEAT :: Jwt 디렉토리 구현

---
+++ src/main/java/com/laundering/launering_server/common/jwt/JwtProperties.java
@@ -0,0 +1,19 @@
+package com.laundering.launering_server.common.jwt;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+    private String issuer;
+    private String clientSecret;
+    private int tokenExpire;
+    private int refreshTokenExpire;
+}
+
