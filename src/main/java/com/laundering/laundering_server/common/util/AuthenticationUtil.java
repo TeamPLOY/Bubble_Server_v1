c984bde3feb68fa4c6467dd57da76da85f8cb1fe
noahmik <s46422574@gmail.com>
2024-09-13 22:00:31 +0900

    인증 관련 로직 수정

--- src/main/java/com/laundering/laundering_server/common/util/AuthenticationUtil.java
+++ src/main/java/com/laundering/laundering_server/common/util/AuthenticationUtil.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.util.Objects;
 
 /**
  * 인증 관련 유틸리티 클래스입니다.
@@ -20,6 +19,8 @@ public class AuthenticationUtil {
      @return 인증된 사용자의 회원 ID. 인증되지 않은 경우 null을 반환합니다.
      **/
     public static Long getMemberId() {
+        var anonymous = String.valueOf(isAnonymous());
+        log.warn("known : "+anonymous);
         if (isAnonymous()) {
             return null;
         }
@@ -37,21 +38,14 @@ public class AuthenticationUtil {
     }
 
     /**
-     * 주어진 회원 ID가 현재 인증된 사용자와 일치하는지 확인합니다.
-     *
-     * @param memberId 확인할 회원 ID
-     * @return 일치하면 true, 그렇지 않으면 false를 반환합니다.
-     */
-    public static boolean isValidAccess(Long memberId) {
-        return Objects.equals(AuthenticationUtil.getMemberId(), memberId);
-    }
-
-    /**
      * 현재 인증 객체를 가져옵니다.
      *
      * @return 현재 인증 객체
      */
     private static Authentication getAuthentication() {
-        return SecurityContextHolder.getContext().getAuthentication();
-    }
+        var context = SecurityContextHolder.getContext();
+        log.info("Context :"+ context);
+        var result = context.getAuthentication();
+        log.info("result : " + result);
+        return result;    }
 }
