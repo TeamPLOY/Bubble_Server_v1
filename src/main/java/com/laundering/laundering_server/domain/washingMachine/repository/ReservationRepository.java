bff3be1328226aef910c1bdf1e8040150f032a6a
한태영 <s46422574@gmail.com>
2024-09-14 05:18:13 +0900

    FEAT :: ReservationRepository 구현

--- src/main/java/com/laundering/laundering_server/domain/washingMachine/repository/ReservationRepository.java
+++ src/main/java/com/laundering/laundering_server/domain/washingMachine/repository/ReservationRepository.java
@@ -1,2 +1,17 @@
-package com.laundering.laundering_server.domain.washingMachine.repository;public class ReservationRepository {
+package com.laundering.laundering_server.domain.washingMachine.repository;
+
+
+import com.laundering.laundering_server.domain.washingMachine.model.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.time.LocalDate;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    // userId로 예약을 조회하는 메소드
+    Optional<Reservation> findByUserId(Long userId);
+
+    Optional<Reservation> findByUserIdAndDate(Long userId, LocalDate date);
 }
+
+
