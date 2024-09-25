f6de14d176e7970058bd9640044759f07644f900
noahmik <s46422574@gmail.com>
2024-09-25 19:59:55 +0900

    REFACTOR :: LocalDate를 LocalDateTime으로 변경

--- src/main/java/com/laundering/laundering_server/domain/facade/WashingMachineFacade.java
+++ src/main/java/com/laundering/laundering_server/domain/facade/WashingMachineFacade.java
@@ -1,13 +1,15 @@
 package com.laundering.laundering_server.domain.facade;
 
+import com.laundering.laundering_server.domain.washingMachine.model.dto.response.ReservationSummaryResponse;
 import com.laundering.laundering_server.domain.washingMachine.model.dto.response.WashingMachineResponse;
+import com.laundering.laundering_server.domain.washingMachine.model.entity.Reservation;
 import com.laundering.laundering_server.domain.washingMachine.service.ReservationService;
 import com.laundering.laundering_server.domain.washingMachine.service.WashingMachineService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -21,18 +23,18 @@ public class WashingMachineFacade {
     }
 
     @Transactional
-    public void reservation(Long id, LocalDate date) {
+    public void reservation(Long id, LocalDateTime date) {
         reservationService.reservation(id,date);
     }
 
     @Transactional
-    public void cancelReservation(Long id, LocalDate date) {
+    public void cancelReservation(Long id, LocalDateTime date) {
         reservationService.cancelReservation(id,date);
     }
 
     @Transactional
-    public void getReservation(Long id) {
-        reservationService.getReservation(id);
+    public List<ReservationSummaryResponse> getReservation(Long id) {
+        return reservationService.getReservation(id);
     }
 
 }
--- src/main/java/com/laundering/laundering_server/domain/washingMachine/repository/ReservationRepository.java
+++ src/main/java/com/laundering/laundering_server/domain/washingMachine/repository/ReservationRepository.java
@@ -4,6 +4,7 @@ package com.laundering.laundering_server.domain.washingMachine.repository;
 import com.laundering.laundering_server.domain.washingMachine.model.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.time.LocalDate;
@@ -12,9 +13,12 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     // userId로 예약을 조회하는 메소드
     Optional<Reservation> findByUserId(Long userId);
 
-    Optional<Reservation> findByUserIdAndDate(Long userId, LocalDate date);
+    Optional<Reservation> findByUserIdAndDate(Long userId, LocalDateTime date);
 
     List<Reservation> findByWashingRoom(String washingRoom);
+
+    List<Reservation> findByWashingRoomAndDateBetween(String washingRoom, LocalDateTime start, LocalDateTime end);
+
 }
 
 
