7a7be879407470ee1b8ca357cc4c11814e8d7ecc
noahmik <s46422574@gmail.com>
2024-09-25 20:00:45 +0900

    FEAT :: ReservationSummaryResponse 구현

--- src/main/java/com/laundering/laundering_server/domain/washingMachine/model/dto/response/ReservationSummaryResponse.java
+++ src/main/java/com/laundering/laundering_server/domain/washingMachine/model/dto/response/ReservationSummaryResponse.java
@@ -1,2 +1,9 @@
-package com.laundering.laundering_server.domain.washingMachine.model.dto.response;public record ReservationSummaryResponse() {
+package com.laundering.laundering_server.domain.washingMachine.model.dto.response;
+
+import java.time.LocalDate;
+
+public record ReservationSummaryResponse(
+        LocalDate date, // 날짜를 LocalDate로 사용 (시간 제외)
+        Long userCount // 예약한 유저 수
+) {
 }
