842a56796cea359861be392ed65ec0d9354fe0ea
한태영 <s46422574@gmail.com>
2024-09-26 05:08:34 +0900

    REFACTOR :: dto 재정의

--- src/main/java/com/laundering/laundering_server/domain/washingMachine/model/dto/response/ReservationSummaryResponse.java
+++ src/main/java/com/laundering/laundering_server/domain/washingMachine/model/dto/response/ReservationSummaryResponse.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public record ReservationSummaryResponse(
         LocalDate date, // 날짜를 LocalDate로 사용 (시간 제외)
+        String day, // 무슨 요일인지
         Long userCount // 예약한 유저 수
 ) {
 }
