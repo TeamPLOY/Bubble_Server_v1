7c9e633d1d531741f9cde0e554ef9fee1e9e6a3e
noahmik <s46422574@gmail.com>
2024-11-07 14:43:16 +0900

    feat :: deleteRequest 추가

--- src/main/java/com/laundering/laundering_server/domain/member/model/dto/request/DeleteRequest.java
+++ src/main/java/com/laundering/laundering_server/domain/member/model/dto/request/DeleteRequest.java
@@ -1,2 +1,7 @@
-package com.laundering.laundering_server.domain.member.model.dto.request;public record DeleteRequest() {
+package com.laundering.laundering_server.domain.member.model.dto.request;
+
+public record DeleteRequest(
+        String email,
+        String password
+) {
 }
