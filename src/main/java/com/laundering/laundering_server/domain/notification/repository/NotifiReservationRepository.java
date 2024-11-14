b33f9406fc7902b794d81b829d6f3b42195ba2af
noahmik <s46422574@gmail.com>
2024-11-14 19:17:35 +0900

    feat :: findByMachine 구현

--- src/main/java/com/laundering/laundering_server/domain/notification/repository/NotifiReservationRepository.java
+++ src/main/java/com/laundering/laundering_server/domain/notification/repository/NotifiReservationRepository.java
@@ -4,8 +4,12 @@ package com.laundering.laundering_server.domain.notification.repository;
 import com.laundering.laundering_server.domain.notification.model.entity.NotifiReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface NotifiReservationRepository extends JpaRepository<NotifiReservation, Long> {
     Optional<NotifiReservation> findByUserIdAndMachine(Long userId, String machine);
+
+    List<NotifiReservation> findByMachine(String machine);
+
 }
